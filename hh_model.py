6ed9e235aa83119680d7bc908b382a0d22c3e834
jamesz3ng <jameszeng23@gmail.com>
2025-11-24 15:30:42 +1300

    add constants

---
+++ hh_model.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# constants
+C_m = 1.0
+g_Na = 120.0
+g_K = 36.0
+g_L = 0.3
+
+# reverse potential (mV)
+E_Na = 50.0
+E_K = -77.0
+E_L = -54.387
+
+# rate functions
+# k activation
+
+
+
+
+
+if __name__ == "__main__":
