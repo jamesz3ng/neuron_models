c78a055aea000bd1d69e9386fa8ff624a492626b
jamesz3ng <jameszeng23@gmail.com>
2025-11-25 12:50:29 +1300

    feat: update plotting to also display gate open possibilities

--- hh_model.py
+++ hh_model.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# https://goldmanlab.faculty.ucdavis.edu/wp-content/uploads/sites/263/2016/07/HodgkinHuxley.pdf
+# values taken from above converted to SI units
+
 # constants
 C_m = 1.0  # nF/mm^2
 g_Na = 120.0  # uS/cm^2
@@ -51,8 +54,6 @@ def main():
     n = np.zeros(len(t))
 
     # set initial resting positions
-    # https://goldmanlab.faculty.ucdavis.edu/wp-content/uploads/sites/263/2016/07/HodgkinHuxley.pdf
-    # values taken from above
     V[0] = -65.0
     m[0] = 0.0529
     h[0] = 0.5961
@@ -61,6 +62,7 @@ def main():
     I_inj = np.zeros(len(t))
     I_inj[int(20 / dt) : int(21 / dt)] = 200
 
+    # Using the Euler forward method
     for i in range(1, len(t)):
         V_old = V[i - 1]
         m_old = m[i - 1]
@@ -82,21 +84,33 @@ def main():
         dV = (I_inj[i - 1] - I_Na - I_K - I_L) / C_m
         V[i] = V_old + dV * dt
 
-    print(V)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 6), sharex=False)
 
-    plt.figure(figsize=(10, 6))
+    ax1.plot(t, V, color="blue")
+    # ax1.plot(t, m_p, color="green", alpha=0.3, label="m gate")
+    # ax1.plot(t, n_p, color="red", alpha=0.3, label="n gate")
+    # ax1.plot(t, h_p, color="purple", alpha=0.3, label="h gate")
 
-    plt.subplot(2, 1, 1)
-    plt.plot(t, V, "b", lw=2)
-    plt.ylabel("Membrane Potential (mV)")
-    plt.title("Hodgkin-Huxley Action Potential")
-    plt.grid(True)
+    ax1_right = ax1.twinx()
+    ax1_right.set_ylabel("Gating Probability")
+    ax1_right.set_ylim(0, 1.1)
 
-    plt.subplot(2, 1, 2)
-    plt.plot(t, I_inj, "r", lw=2)
-    plt.xlabel("Time")
-    plt.ylabel("Injected Current")
-    plt.grid(True)
+    gates = {"m": (m, "green"), "n": (n, "red"), "h": (h, "purple")}
+
+    for name, (data, color) in gates.items():
+        ax1_right.plot(t, data, color=color, alpha=0.3, label=f"{name} gate")
+
+    ax1_right.legend()
+
+    ax1.set_xlabel("Time")
+    ax1.set_ylabel("Membrane Potential (mV)")
+    ax1.set_title("Action Potential Propagation")
+    ax1.grid(True)
+
+    ax2.plot(t, I_inj, color="Red")
+    ax2.set_xlabel("Time")
+    ax2.set_ylabel("Injeted Current")
+    ax2.grid(True)
 
     plt.tight_layout()
     plt.show()
